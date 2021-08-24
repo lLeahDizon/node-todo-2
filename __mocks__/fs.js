46271fa8cc7b0500d317920505841d48edfa5061
杨海涛 <haitao.yht@yibao.com>
2021-08-24 20:19:56 +0800

    feature: 测试写文件

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -3,21 +3,38 @@ const _fs = jest.requireActual('fs')
 
 Object.assign(fs, _fs)
 
-const mocks = {}
+let readMocks = {}
 
-fs.setMock = (path, error, data) => {
-  mocks[path] = [error, data]
+fs.setReadFileMock = (path, error, data) => {
+  readMocks[path] = [error, data]
 }
 
-// fs.readFile('xxx', fn)
 fs.readFile = (path, options, callback) => {
   if (callback === undefined) {
     callback = options
   }
-  if (path in mocks) {
-    callback(...mocks[path])
+  if (path in readMocks) {
+    callback(...readMocks[path])
   } else
     _fs.readFile(path, options, callback)
+}
+
+let writeMocks = {}
+
+fs.setWriteFileMock = (path, fn) => {
+  writeMocks[path] = fn
+}
+
+fs.writeFile = (path, data, options, callback) => {
+  if (path in writeMocks) {
+    writeMocks[path](path, data, options, callback)
+  } else
+    _fs.writeFile(path, data, options, callback)
+}
+
+fs.clearMocks = () => {
+  readMocks = {}
+  writeMocks = {}
 }
 
 module.exports = fs
--- __tests__/db.spec.js
+++ __tests__/db.spec.js
@@ -3,10 +3,23 @@ const fs = require('fs')
 jest.mock('fs')
 
 describe('db', () => {
+  afterEach(() => {
+    fs.clearMocks()
+  })
   it('can read', async () => {
     const data = [{title: 'hi', done: true}]
-    fs.setMock('/xxx', null, JSON.stringify(data))
+    fs.setReadFileMock('/xxx', null, JSON.stringify(data))
     const list = await db.read('/xxx')
     expect(list).toStrictEqual(data)
+  })
+  it('can write', async () => {
+    let fakeFile = ''
+    fs.setWriteFileMock('/yyy', (path, data, callback) => {
+      fakeFile = data
+      callback(null)
+    })
+    const list = [{title: '买水', done: true}, {title: '买饮料', done: true}]
+    await db.write(list, '/yyy')
+    expect(fakeFile).toBe(JSON.stringify(list) + '\n')
   })
 })
